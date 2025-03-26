47170cc52588025d1abdc43f42866f2f9535fa39
Harold Kim <harold.kim@ucalgary.ca>
2025-03-26 11:41:14 -0600

    commit

---
+++ ex2.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+
+class Graph:
+    def addNode(data):
+        pass
+    def removeNode(data):
+        pass
+    def addEdge(n1, n2, weight):
+        pass
+    def removeEdge(n1, n2):
+        pass
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
